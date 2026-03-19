ddb9090c119ee9fa00f930d05e674ffc787e5adf
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-03-19 11:55:29 -0300

    Add files via upload

---
+++ trabajo práctico número 2.cs
@@ -0,0 +1,27 @@
+﻿if(25 > 30) { Console.WriteLine("A1: verdadero"); } else { Console.WriteLine("A1: falso"); }
+if(14 <= 14) { Console.WriteLine("A2: verdadero"); } else { Console.WriteLine("A2: falso"); }
+if(0 == 0) { Console.WriteLine("A3: verdadero"); } else { Console.WriteLine("A3: falso"); }
+if(10 != 10) { Console.WriteLine("A4: verdadero"); } else { Console.WriteLine("A4: falso"); }
+if(8 >= 3) { Console.WriteLine("A5: verdadero"); } else { Console.WriteLine("A5: falso"); }
+if(15 < 15) { Console.WriteLine("A6: verdadero"); } else { Console.WriteLine("A6: falso"); }
+if(100 != 50) { Console.WriteLine("A7: verdadero"); } else { Console.WriteLine("A7: falso"); }
+if(7 == 7.0) { Console.WriteLine("A8: verdadero"); } else { Console.WriteLine("A8: falso"); }
+if(0.5 > 0) { Console.WriteLine("A9: verdadero"); } else { Console.WriteLine("A9: falso"); }
+if (-5 < -2) { Console.WriteLine("A10: verdadero"); } else { Console.WriteLine("A10: falso"); }
+if(5 > 3 && 10 < 20) { Console.WriteLine("B1: verdadero"); } else { Console.WriteLine("B1: falso"); }
+if(8 == 10 || 5 != 2) { Console.WriteLine("B2: verdadero"); } else { Console.WriteLine("B2: falso"); }
+if(!(10 == 10)) { Console.WriteLine("B3: verdadero"); } else { Console.WriteLine("B3: falso"); }
+if(15 > 10 && !(5 > 10)) { Console.WriteLine("B4: verdadero"); } else { Console.WriteLine("B4: falso"); }
+if(10 + 2 > 10 || 5 * 2 < 8) { Console.WriteLine("B5: verdadero"); }else { Console.WriteLine("B5: falso"); }
+
+int temperatura = 35;
+bool humedad = false;
+if(temperatura > 30 && humedad == true) { Console.WriteLine("B6: verdadero"); } else { Console.WriteLine("B6: falso"); }
+
+int edad = 16;
+if(!(edad >= 18)) { Console.WriteLine("B7: verdadero"); } else { Console.WriteLine("B7: falso"); }
+if(10 / 3 == 3 && 10 % 3 == 1) { Console.WriteLine("B8: verdadero"); } else { Console.WriteLine("B8: falso"); }
+
+string nombre = "Ana";
+if(nombre == "Juan" || nombre == "Ana") { Console.WriteLine("B9: verdadero"); } else { Console.WriteLine("B9: false"); }
+if ((5 * 2)+ 10 > 15 && 20 / 4 == 5) { Console.WriteLine("B10: verdadero"); } else { Console.WriteLine("B10: false"); }

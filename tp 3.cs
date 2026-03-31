0e97dffe833d7388e4895f702b50ac535f698360
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-03-31 13:07:56 -0300

    tp 3.cs

--- tp 3.cs
+++ tp 3.cs
@@ -1,6 +1,9 @@
-﻿// ejercicio 1
+// ejercicio 1
+using System.ComponentModel;
+
 Console.WriteLine("Ingrese un número entero");
-string ejercicio1(int N) {
+string ejercicio1(int N)
+{
 
     string resultado;
     if (N > 0) { resultado = "positivo"; }
@@ -9,7 +12,7 @@ string ejercicio1(int N) {
     {
         resultado = "es 0";
     }
-        return resultado;
+    return resultado;
 }
 
 Console.WriteLine(ejercicio1(Convert.ToInt32(Console.ReadLine())));
@@ -21,8 +24,9 @@ string ejercicio2(int edad)
     string fiesta;
 
     if (edad >= 18) { fiesta = "¡Bienvenido a la fiesta!"; }
-    else {fiesta = "Lo siento, eres muy joven"; }
-    return fiesta;}
+    else { fiesta = "Lo siento, eres muy joven"; }
+    return fiesta;
+}
 Console.WriteLine(ejercicio2(Convert.ToInt32(Console.ReadLine())));
 
 // ejercicio 3
@@ -32,8 +36,31 @@ string ejercicio3(string contraseña)
     string confirmacion;
     if (contraseña == "python123") { confirmacion = "¡Contraseña correcta! Acceso concedido."; }
     else { confirmacion = "¡Contraseña incorrecta, Autodestrucción en 5 minutos"; }
-    return confirmacion; 
+    return confirmacion;
 }
 Console.WriteLine(ejercicio3(Console.ReadLine()));
 
 // ejercicio 4
+Console.WriteLine("Ingrese un número entero");
+string ejercicio4(int NUM)
+{
+    string verificacion;
+    if (NUM % 2 == 0) { verificacion = "El número es par"; }
+    else { verificacion = "El número es impar"; }
+    return verificacion;
+}
+Console.WriteLine(ejercicio4(Convert.ToInt32(Console.ReadLine())));
+
+//ejercicio 5
+Console.WriteLine("Cuál es tu edad?");
+Console.WriteLine("compro pochoclos?");
+string ejercicio5(int numero, string compro)
+{   
+    string validacion;
+    if (numero > 65 && compro == "si") { validacion = "¡Felicidades! Tienes entrada gratuita al cine."; }
+    else { validacion = "Comprá la entrada o raja de acá"; }
+    return validacion;
+}
+Console.WriteLine(ejercicio5(Convert.ToInt32(Console.ReadLine()),Console.ReadLine()));
+
+

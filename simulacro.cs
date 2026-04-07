12ae54ed3401cdf0fd9c14e38f3627bb12755a3f
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-04-07 13:28:20 -0300

    Add files via upload

---
+++ simulacro.cs
@@ -0,0 +1,11 @@
+﻿
+string nombre = Console.ReadLine();
+double promedio = Convert.ToDouble(Console.ReadLine());
+int distancia = Convert.ToInt32(Console.ReadLine());
+
+bool DeterminarBeca(double promedio, int distancia)
+{
+    if (promedio >= 8.5 || distancia > 50) { return true;} 
+    else { return false; }
+}
+bool resultado = DeterminarBeca(double promedio, int distancia)

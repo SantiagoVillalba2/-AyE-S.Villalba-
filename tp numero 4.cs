b035b9df5b697232b701636123a4af92c7558c90
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-04-07 11:46:22 -0300

    tp numero 4.cs

--- tp numero 4.cs
+++ tp numero 4.cs
@@ -5,14 +5,17 @@ for (int x = 1; x <= 10; x++) { Console.WriteLine(x); }
 for (int x = 1; x <= 5; x++) { Console.WriteLine("¡Hola, mundo!"); }
 
 // ejercicio 3
-for (int x = 2; x <= 20; x++) { Console.WriteLine(x); }
+for (int x = 2; x <= 20; x+=2){
+    Console.WriteLine(x); }
 
 // ejercicio 4
-for (int I = 1; I <= 10; I++) { Console.WriteLine((7 * I)); }
+for (int I = 1; I <= 10; I++) { Console.WriteLine(7 * I); }
 
 // ejercicio 5
-for (int A = 0; A <= 15; A++)
+int resultado = 0;
+for (int A = 1; A <= 5; A++)
 {
-    A = A + 1;
-    Console.WriteLine(A);
+    resultado += A;
+    
+    Console.WriteLine(resultado);
 };

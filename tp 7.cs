2c75cc2250b98af3cc8d3c85597bd55c1a0f8a43
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-04-16 11:50:27 -0300

    Add files via upload

---
+++ tp 7.cs
@@ -0,0 +1,53 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+
+bool esPrimo(int n)
+{
+    if(n <= 1)
+    {
+        return false;
+    }
+    if(n % 2 == 0)
+    {
+        return false;
+    } 
+    if(n == 2)
+    {
+        return true;
+    }
+    for(int x = 3; x < n; x = +2)
+    {
+        if(n % x == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool resultado = esPrimo(n);
+if(resultado == true)
+{
+    Console.WriteLine("Es primo");
+}
+else
+{
+    Console.WriteLine("No es primo");
+}
+// ejercicio 2
+Console.WriteLine("ingrese un número entero NO negativo");
+int numero = Convert.ToInt32(Console.ReadLine());
+int resultadoo = 1;
+if (n < 0)
+{
+    Console.WriteLine("Ingrese un número positivo");
+}
+else
+{
+    for (int xx = 1; xx <= numero; xx++)
+    {
+        resultadoo *= xx;
+    }
+    Console.Write("El factorial de ");
+    Console.Write(numero);
+    Console.Write(" es ");
+    Console.Write(resultadoo);
+}

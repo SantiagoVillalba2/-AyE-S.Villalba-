ad1998aedff5df7a876bf206ffe4d33129b648b0
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-04-23 11:57:13 -0300

    tp 8.cs

--- tp 8.cs
+++ tp 8.cs
@@ -1,12 +1,14 @@
-﻿Console.WriteLine("ingrese una frase");
+﻿// ejercicio 1
+
+Console.WriteLine("ingrese una frase");
 string frase = Console.ReadLine();
 string vocales = "aeiouAEIOU";
 int contador = 0;
-foreach(char letra in frase)
+foreach (char letra in frase)
 {
-    foreach(char vocal in vocales)
+    foreach (char vocal in vocales)
     {
-        if(letra == vocal)
+        if (letra == vocal)
         {
             contador++;
             break;
@@ -19,18 +21,19 @@ Console.WriteLine($"La cantidad de vocales que hay es de {contador}");
 // ejercicio 2
 Console.WriteLine("ingrese una palabra o frase");
 string palabra = Console.ReadLine();
-for(int i = palabra.Length - 1; i > -1; i--)
+for (int i = palabra.Length - 1; i > -1; i--)
 {
     char invertido = palabra[i];
-    Console.WriteLine(invertido);
+    Console.Write(invertido);
 }
 
 // ejercicio 3
+Console.WriteLine("");
 Console.WriteLine("ingrese un número");
 string numero = Console.ReadLine();
 int aux = 0;
 int aux2;
-foreach(char N in numero)
+foreach (char N in numero)
 {
     aux2 = Convert.ToInt32("" + N);
     aux += aux2;
@@ -38,21 +41,38 @@ foreach(char N in numero)
 Console.WriteLine(aux);
 
 //ejercicio 4
-string reemplazo;
+string parrafito = "hola chau reconozco palabra";
 Console.WriteLine("ingresa una palabra prohibída");
 string palabra1 = Console.ReadLine();
 Console.WriteLine("ingresa una palabra de reemplazo");
 string palabra2 = Console.ReadLine();
-reemplazo = palabra1.Replace(palabra1, palabra2);
+string reemplazo = parrafito.Replace(palabra1, palabra2);
 Console.WriteLine(reemplazo);
 
+
 //ejercicio 5
-Console.WriteLine("ingrese su nombre");
-string nombre = Console.ReadLine();
-Console.WriteLine("ingrese su apellido");
-string apellido = Console.ReadLine();
+Console.WriteLine("ingrese su nombre completo");
+string completo = Console.ReadLine();
+string[] a = completo.Split(" ");
+string nombre = a[0];
+string apellido = a[1];
+Console.WriteLine($"Iniciales: {nombre[0]}.{apellido[0]}");
+Console.WriteLine($"Primer nombre: {nombre}");
+Console.WriteLine($"Apellido: {apellido}");
 
-foreach (char letra1 in nombre)
+// ejercicio 6
+Console.WriteLine("ingrese una palabra");
+string pa = Console.ReadLine();
+string invertida = "";
+foreach(char le in pa)
 {
-
+    invertida = le + invertida;
+}
+if(invertida == pa)
+{
+    Console.WriteLine("Es palíndromo");
+}
+else
+{
+    Console.WriteLine("No es palíndromo");
 }

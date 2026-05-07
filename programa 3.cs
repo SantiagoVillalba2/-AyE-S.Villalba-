4c27c2f282a4adc8734ef6795104494e028ac3bd
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-05-07 10:29:32 -0300

    Add files via upload

---
+++ programa 3.cs
@@ -0,0 +1,57 @@
+﻿string palabra = "playstation";
+string guiones = "";
+string nuevo = "";
+char letra = ' ';
+int intentos = 7;
+bool adivina = true;
+bool encontro = false;
+
+
+for (int i = 0; i < palabra.Length; i++)
+{
+    guiones += "_";
+}
+for (int i = 0; i < palabra.Length; i++)
+{
+    while (letra != palabra[i] && !adivina)
+    {
+        Console.WriteLine($"palabra: {guiones}");
+        Console.WriteLine($"intentos restantes: {intentos}");
+        Console.WriteLine("Ingresá una letra");
+        letra = Convert.ToChar(Console.ReadLine());
+
+        if (letra == palabra[i])
+        {
+            nuevo += letra;
+            encontro = true;
+        }
+        else
+        {
+            nuevo += guiones[i];
+        }
+        guiones = nuevo;
+        if (!encontro)
+        {
+            intentos--;
+            Console.WriteLine("Letra incorrecta.");
+        }
+        else
+        {
+            Console.WriteLine("Letra encontrada.");
+        }
+        if (guiones == palabra)
+        {
+            adivina = true;
+        }
+    }
+}
+
+Console.WriteLine("");
+if (adivina)
+{
+    Console.WriteLine($"Ganaste, la palabra era: {palabra}");
+}
+else
+{
+    Console.WriteLine($"Perdiste, la palabra era: {palabra}");
+}

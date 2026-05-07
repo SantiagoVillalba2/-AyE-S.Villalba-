7b42b5cf7cb0e6736ad3b64b5662665891a04f30
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-05-07 12:02:34 -0300

    Add files via upload

---
+++ tp 11.cs
@@ -0,0 +1,17 @@
+﻿// ejercicio 1
+
+int contar(int contador)
+{
+    contador = 1;
+    if(contador > 10)
+    {
+        Console.WriteLine(contador);
+        return contador;
+    }
+    else
+    {
+        contar(contador + 1);
+        Console.WriteLine(contador);
+        return contador;
+    }
+}

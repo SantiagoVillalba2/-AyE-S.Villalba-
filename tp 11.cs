b948efcf48d69ca007d437b822bc146e31106059
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-05-19 13:16:05 -0300

    tp 11

--- tp 11.cs
+++ tp 11.cs
@@ -1,17 +1,89 @@
-﻿// ejercicio 1
-
-int contar(int contador)
+﻿Console.WriteLine("Ejercicio 1");
+void for1(int num = 1)
 {
-    contador = 1;
-    if(contador > 10)
+    if(num > 10 )
     {
-        Console.WriteLine(contador);
-        return contador;
+        return;
     }
     else
     {
-        contar(contador + 1);
-        Console.WriteLine(contador);
-        return contador;
+        Console.WriteLine(num);
+        for1(num + 1);
     }
 }
+for1();
+Console.WriteLine("Ejercicio 2");
+void for2(int n = 1, string hola = "¡Hola, mundo!")
+{
+    if(n > 5)
+    {
+        return;
+    }
+    else
+    {
+        Console.WriteLine(hola);
+        for2(n + 1);
+    }
+}
+for2();
+Console.WriteLine("Ejercicio 3");
+void for3(int numm = 2)
+{
+    if(numm > 20)
+    {
+        return;
+    }
+    else
+    {
+        Console.WriteLine(numm);
+        for3(numm + 2);
+    }
+}
+for3();
+Console.WriteLine("Ejercicio 4");
+void for4(int multiplicador = 1)
+{
+    if(multiplicador > 10)
+    {
+        return;
+    }
+    else
+    {
+        int resultado = 7 * multiplicador;
+        Console.WriteLine(resultado);
+        for4(multiplicador + 1);
+    }
+}
+for4();
+Console.WriteLine("Ejercicio 5");
+int for5(int suma = 1)
+{
+    if(suma > 5)
+    {
+        return 0;
+    }
+    else
+    {
+        return suma + for5(suma + 1);
+    }
+}
+int resultadoo = for5();
+Console.WriteLine($"La suma total: {resultadoo}");
+Console.WriteLine("");
+Console.WriteLine("Ejercicio 6");
+void while1()
+{
+    Console.WriteLine("Ingrese una contraseña");
+    string intento = Console.ReadLine();
+    if(intento == "playstation")
+    {
+        Console.WriteLine("Entrando...");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Intentelo de nuevo");
+        while1();
+    }
+}
+while1();

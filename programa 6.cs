7454af37019cf5972c20923ba41aabd98bb57e12
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-06-09 13:26:45 -0300

    Add files via upload

---
+++ programa 6.cs
@@ -0,0 +1,15 @@
+﻿int[,] mapa = new int[10, 10];
+Random aleatorio = new Random();
+int tesoros = 0;
+
+while(tesoros < 3)
+{
+    int filaazar = aleatorio.Next(0, 10);
+    int colazar = aleatorio.Next(0, 10);
+
+    if (mapa[filaazar, colazar] == 0)
+    {
+        mapa[filaazar, colazar] = 1;
+        tesoros++;
+    }
+}

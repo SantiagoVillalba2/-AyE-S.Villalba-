a21c0150c775349a299c77012962d7034ff93d1a
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-07-02 11:36:52 -0300

    Add files via upload

---
+++ ejercicio 1.cs
@@ -0,0 +1,29 @@
+﻿namespace asd
+{
+    internal class Program
+    {
+        public struct Punto2D
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            public Punto2D(int PuntoX, int PuntoY)
+            {
+                x = PuntoX;
+                y = PuntoY;
+            }
+
+            public void mostrar()
+            {
+                Console.Write($"{x}, {y}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Punto2D mipunto = new Punto2D(5, 10);
+
+            mipunto.mostrar();
+        }
+    }
+}
---
+++ ejercicio 2.cs
@@ -0,0 +1,29 @@
+﻿namespace ejercicio2
+{
+    internal class program
+    {
+        public struct Dimensiones
+        {
+            public int ancho { get; set; }
+            public int alto { get; set; }
+
+            public Dimensiones(int Ancho, int Alto)
+            {
+                Ancho = ancho;
+                Alto = alto;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dimensiones dimension1 = new Dimensiones(10, 20);
+            Dimensiones dimension2 = dimension1;
+            dimension2.ancho = 99;
+
+            Console.WriteLine("Ancho y alto de dimension 1");
+            Console.WriteLine($"Ancho: {dimension1.ancho}, alto: {dimension1.alto}");
+            Console.WriteLine("Ancho y alto de dimension 2");
+            Console.WriteLine($"Ancho: {dimension2.ancho}, alto: {dimension2.alto}");
+        }
+    }
+}

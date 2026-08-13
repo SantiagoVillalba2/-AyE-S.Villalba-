533aff96eee3e2bb2d6419b534fc948c6e7a3e12
SantiagoVillalba2 <ricardoleiva1943@gmail.com>
2026-08-13 11:34:41 -0300

    Add files via upload

---
+++ ejercicio 4.cs
@@ -0,0 +1,42 @@
+﻿using System.Transactions;
+
+namespace ejercicio_4
+{
+    internal class Program
+    {
+        public struct estudiante
+        {
+            public string Nombre { get; set; }
+            public double[] Notas { get; set; }
+
+            public estudiante(string nombre, double[] notas)
+            {
+                Nombre = nombre;
+                Notas = notas;
+            }
+
+            public double CalcularPromedio()
+            {
+                double suma = 0;
+
+                foreach(double n in Notas)
+                {
+                    suma += n;
+                }
+
+                return suma / Notas.Length;
+            }
+        }
+        static void Main(string[] args)
+        {
+            double[] notasestudiante = new double[] { 8.5, 3.0, 5.5 };
+
+            estudiante alumno = new estudiante("Pepe", notasestudiante);
+            double promediofinal = alumno.CalcularPromedio();
+
+            Console.WriteLine("Calificaciones");
+            Console.WriteLine($"Estudiante: {alumno.Nombre}");
+            Console.WriteLine($"Promedio final: {promediofinal}");
+        }
+    }
+}
